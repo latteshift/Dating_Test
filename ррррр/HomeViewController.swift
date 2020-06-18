8d6c94fc85f7919407f49e54a13f50af63d86236
Inga Marchenko <ingamarchenko@Went-Bananas-Mac.local>
2020-06-18 18:54:37 +0300

    commit

--- ррррр/HomeViewController.swift
+++ ррррр/HomeViewController.swift
@@ -10,13 +10,30 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
-    @IBOutlet weak var firstNameLabel: UILabel!
-    
     @IBOutlet weak var firstNameTextField: UITextField!
+    
+    @IBOutlet weak var secondNameTextField: UITextField!
+    
+    @IBOutlet weak var cityTextField: UITextField!
+    
+    @IBOutlet weak var jobTextField: UITextField!
+    
+    @IBOutlet weak var relationshipTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let name = firstNameTextField.text
-        print(name!)
+     
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func handleSignUpButton(_ sender: UIButton) {
+        let firstName = firstNameTextField.text
+        let secondName = secondNameTextField.text
+        let city = cityTextField.text
+        let job = jobTextField.text
+        let relationship = relationshipTextField.text
+    
     }
 }
